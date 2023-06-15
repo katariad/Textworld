cdf1ecb4335c66b79066b317cc2b30ef2b97bd1c
katariad <katariad2016@gmail.com>
2023-06-15 15:28:18 +0530

    first commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,7 +8,10 @@ export default function Navbar(props) {
   <div className="container-fluid ">
 
     <a className="navbar-brand " href="">{props.title}</a>
- 
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
